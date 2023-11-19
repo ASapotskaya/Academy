ada0c13c1dd193bbb45c97436eba22565d9d24b8
Laksiya <Laksi@LAPTOP-3K4DPE8K>
2023-11-19 22:44:31 +0400

    Files

--- Files/Program.cs
+++ Files/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿//#define WRITE_TO_FILE
+#define READ_TO_FILE
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; // Потоки ввода/вывода
 
 namespace Files
 {
@@ -10,6 +14,40 @@ namespace Files
     {
         static void Main(string[] args)
         {
+            string current_directory = Directory.GetCurrentDirectory();
+            string filename = "File.txt";
+            Console.WriteLine(current_directory);
+#if WRITE_TO_FILE
+            Console.WriteLine("Hello files");
+            //StreamWriter sw = new StreamWriter(filename);
+            //sw.WriteLine("Hello files");
+            //sw.Close();
+
+            StreamWriter aw = File.AppendText(filename);
+            aw.WriteLine("Append");
+            aw.Close();
+
+
+            string cmd = $"{current_directory}\\{filename}";
+            System.Diagnostics.Process.Start("notepad", cmd); 
+#endif
+            //Exception - это ошибка на этапе выполнения
+            try
+            {
+                //throw new Exception("Нет такого файла"); ключевое слово throw  бросает исключение
+                StreamReader sr = new StreamReader("File1.txt");
+                while (!sr.EndOfStream)
+                {
+                    string buffer = sr.ReadLine();
+                    Console.WriteLine(buffer);
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
         }
     }
 }
