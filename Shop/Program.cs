bc652253cb14e34022d158242203fa1440854aad
Laksiya <Laksi@LAPTOP-3K4DPE8K>
2023-11-24 20:26:57 +0400

    Switch added

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -33,12 +33,13 @@ namespace Shop
             Goods[] gooods = { bref, apple };
 
             Console.WriteLine("Выберите товар");
-
+            Console.WriteLine("1. БрэфЭктив. ");
+            Console.WriteLine("2. Яблоко. ");
             int number = Convert.ToInt32(Console.ReadLine());
             switch(number)
             {
-                case 8: gooods[0].MenuPrint(); break;
-                case 9: gooods[1].MenuPrint(); break;
+                case 1: gooods[0].MenuPrint(); break;
+                case 2: gooods[1].MenuPrint(); break;
             }
 
 
